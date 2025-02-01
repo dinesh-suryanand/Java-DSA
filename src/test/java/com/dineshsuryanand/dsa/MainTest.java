71db253b1ae1ebb9f01d4c3adc6a47ead37a2df8
dinesh suryanand <dineshsuryanand.edu@gmail.com>
2025-02-01 10:04:47 +0530

    Initial commit: Set up Java DSA project with Gradle

---
+++ src/test/java/com/dineshsuryanand/dsa/MainTest.java
@@ -0,0 +1,12 @@
+package com.dineshsuryanand.dsa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1, "Basic Math Test");
+    }
+}
