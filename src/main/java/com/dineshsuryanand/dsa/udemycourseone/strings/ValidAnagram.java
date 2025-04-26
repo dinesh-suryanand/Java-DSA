722d37f92d3830534a54bd8efde444e291d095b1
Dinesh Suryanand thinkpad <dineshsuryanand.edu@gmail.com>
2025-04-26 15:00:41 +0530

    completed upto valid anagram

--- src/main/java/com/dineshsuryanand/dsa/udemycourseone/strings/ValidAnagram.java
+++ src/main/java/com/dineshsuryanand/dsa/udemycourseone/strings/ValidAnagram.java
@@ -5,6 +5,15 @@ public class ValidAnagram {
     public static boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
 
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) freq[ch - 'a']++;
+        for (char ch : t.toCharArray()) freq[ch - 'a']--;
+
+        for (int count: freq) {
+            if(count != 0) return false;
+        }
+
 
         return true;
     }
